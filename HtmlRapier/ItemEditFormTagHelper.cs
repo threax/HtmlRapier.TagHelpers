c9a6ca020be80207b8871f37e6be758213224703
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-28 11:30:25 -0400

    Changed the ItemEditorFormTagHelper to insert any elements the user defines into the form.

--- HtmlRapier/ItemEditFormTagHelper.cs
+++ HtmlRapier/ItemEditFormTagHelper.cs
@@ -19,6 +19,15 @@ namespace HtmlRapier.TagHelpers
             output.Attributes.Add(new TagHelperAttribute(Binding, Function));
             output.Attributes.Add(new TagHelperAttribute("data-hr-form", Name));
             output.PreContent.AppendHtml(String.Format(StartContent, Text, ToggleName));
+            if (DynamicAfterStatic)
+            {
+                output.PostContent.AppendHtml(DynamicContent);
+            }
+            else
+            {
+                output.PreContent.AppendHtml(DynamicContent);
+            }
+            output.PostContent.AppendHtml(String.Format(EndContent, Text, ToggleName));
         }
 
         /// <summary>
@@ -40,10 +49,19 @@ namespace HtmlRapier.TagHelpers
 
         public String ToggleName { get; set; } = "mainError"; //1
 
+        /// <summary>
+        /// Set this to true to put dynamic content after static, false to put it before.
+        /// </summary>
+        public bool DynamicAfterStatic { get; set; } = true;
+
         private const String StartContent = @"
             <div class=""modal-body"">
                 <div class=""alert alert-danger hiddenToggler"" role=""alert"" data-hr-toggle=""{1}"" data-hr-view=""{1}"" data-hr-style-on=""display:block;"">{{{{message}}}}</div>
-                <span data-hr-form-end></span>
+";
+
+        private const String DynamicContent = "<span data-hr-form-end></span>";
+
+        private const String EndContent = @"
             </div>
             <div class=""modal-footer"">
                 <button type = ""submit"" class=""btn btn-primary"">{0}</button>
