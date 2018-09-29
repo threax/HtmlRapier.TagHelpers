df10fa63a0f87eb65b98e0e259e23f1f5d12652a
Andrew Piper <andrew@threax.com>
2018-09-29 09:39:38 -0400

    Added an attribute for client config that will expand the path if it starts with ~/ when being written.

---
+++ HtmlRapier.TagHelpers/ExpandHostPathAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlRapier.TagHelpers
+{
+    /// <summary>
+    /// Adding this attribute to a property in a ClientConfig subclass will make
+    /// the url expand to an absolute url if it starts with ~/.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExpandHostPathAttribute : Attribute
+    {
+    }
+}
