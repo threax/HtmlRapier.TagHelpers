6fa0a07de8c892f65d22d6f1d3e3d28f56ae4ed1
Andrew Piper <andrew@threax.com>
2024-08-21 13:11:34 -0400

    Ignore PageBasePath on ClientConfigBase.

--- HtmlRapier.TagHelpers/ClientConfig/ClientConfigBase.cs
+++ HtmlRapier.TagHelpers/ClientConfig/ClientConfigBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HtmlRapier.TagHelpers
@@ -10,6 +11,7 @@ namespace HtmlRapier.TagHelpers
     /// </summary>
     public abstract class ClientConfigBase : IClientConfig
     {
+        [JsonIgnore]
         public String PageBasePath => null;
     }
 }
